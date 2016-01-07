27a35ca3ff7d48cb4178ab8639f90a2c065e5c23
cboutaud <charles.boutaud@gmail.com>
2016-01-07 13:32:01 -0500

    python script matching location type + csv
    python script matching location type + csv

---
+++ loc.py
@@ -0,0 +1,31 @@
+import csv
+
+inputfile = open('mmiw.csv', 'rU')
+csv_reader = csv.DictReader(inputfile)
+
+inputfile2 = open('census.csv', 'rU')
+csv_reader2 = list(csv.DictReader(inputfile2))
+
+# caracter encoding verification
+# with open('census.csv') as f:
+#     for line in f:
+#         print repr(line)
+# data = open('census.csv', 'rb').read()
+# print data.find('\x00')
+# print data.count('\x00')
+
+count = 0
+
+for row in csv_reader:
+	loc = row['loc']
+	for row in csv_reader2:
+		found = False
+		if row['Geographic name'] == loc:
+			print(row['Geographic type'])
+			found = True
+			break
+	if not found:
+		print '-'
+		count +=1
+
+print(count)
